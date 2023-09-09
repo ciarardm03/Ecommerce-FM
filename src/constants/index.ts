30b601c8bca3a340fcb349b4002fff45c79c8888
crdm <ciararosedelamasa.work@gmail.com>
2023-09-09 17:05:09 +0800

    refactor: update image file directort

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,22 +1,22 @@
 export const images = [
   {
     id: 0,
-    src: '/src/assets/images/image-product-1.jpg',
+    src: '/assets/images/image-product-1.jpg',
     alt: 'Product 1',
   },
   {
     id: 1,
-    src: '/src/assets/images/image-product-2.jpg',
+    src: 'assets/images/image-product-2.jpg',
     alt: 'Product 2',
   },
   {
     id: 2,
-    src: '/src/assets/images/image-product-3.jpg',
+    src: 'assets/images/image-product-3.jpg',
     alt: 'Product 3',
   },
   {
     id: 3,
-    src: '/src/assets/images/image-product-4.jpg',
+    src: 'assets/images/image-product-4.jpg',
     alt: 'Product 4',
   },
 ]
